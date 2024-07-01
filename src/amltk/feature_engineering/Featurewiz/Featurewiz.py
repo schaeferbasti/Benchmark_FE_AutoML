2b877e2fcc248a5027146fdb23fa41a2d7eebef6
bastis007 <schaefer.bastian@mail.de>
2024-07-01 10:31:43 +0200

    Upload Featurewiz implementation - works

---
+++ src/amltk/feature_engineering/Featurewiz/Featurewiz.py
@@ -0,0 +1,16 @@
+# https://github.com/AutoViML/featurewiz
+
+import pandas as pd
+from featurewiz import FeatureWiz
+
+
+def get_xxx_features(train_x, train_y, test_x) -> tuple[
+    pd.DataFrame,
+    pd.DataFrame
+]:
+    fwiz = FeatureWiz(feature_engg='', nrows=None, transform_target=True, scalers="std",
+                      category_encoders="auto", add_missing=False, verbose=0, imbalanced=False,
+                      ae_options={})
+    train_x, train_y = fwiz.fit_transform(train_x, train_y)
+    test_x = fwiz.transform(test_x)
+    return train_x, test_x
