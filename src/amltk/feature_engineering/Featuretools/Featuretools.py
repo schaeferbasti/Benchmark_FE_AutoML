d0508c7d6c73baa6eb33e563f5ff60515a21deb1
bastis007 <schaefer.bastian@mail.de>
2024-07-02 18:18:18 +0200

    Featuretools, get out features, but they still make no sense

--- src/amltk/feature_engineering/Featuretools/Featuretools.py
+++ src/amltk/feature_engineering/Featuretools/Featuretools.py
@@ -11,12 +11,21 @@ def get_xxx_features(train_x, train_y, test_x, test_y, name) -> tuple[
     # train_y = pd.DataFrame(train_y, columns=["target"])
     # train_df = pd.concat([train_x, train_y], axis=1)
     train_x.insert(loc=0, column="id", value=train_x.reset_index().index)
+    print(train_x)
     train_y = pd.DataFrame(train_y)
     train_y.insert(loc=0, column="id", value=train_y.reset_index().index)
+    test_x.insert(loc=0, column="id", value=test_x.reset_index().index)
+    test_y = pd.DataFrame(test_y)
+    test_y.insert(loc=0, column="id", value=test_y.reset_index().index)
+
     train_es = ft.EntitySet(id=name)
     train_es = train_es.add_dataframe(
         dataframe_name=name + "_train_x",
         dataframe=train_x,
+    )
+    train_es = train_es.add_dataframe(
+        dataframe_name=name + "_test_x",
+        dataframe=test_x,
     )
     train_es = train_es.add_dataframe(
         dataframe_name="train_y",
@@ -25,17 +34,16 @@ def get_xxx_features(train_x, train_y, test_x, test_y, name) -> tuple[
 
     train_feature_matrix, train_features = ft.dfs(
         entityset=train_es,
-        target_dataframe_name="train_y",
+        target_dataframe_name=name + "_train_x",
     )
 
-    test_x.insert(loc=0, column="id", value=test_x.reset_index().index)
-    test_y = pd.DataFrame(test_y)
-    test_y.insert(loc=0, column="id", value=test_y.reset_index().index)
+
     test_es = ft.EntitySet(id=name)
     test_es = test_es.add_dataframe(
         dataframe_name=name + "_test_x",
         dataframe=test_x,
     )
+    # Shouldn't input test_y to avoid leaking
     test_es = test_es.add_dataframe(
         dataframe_name="test_y",
         dataframe=test_y,
@@ -44,17 +52,14 @@ def get_xxx_features(train_x, train_y, test_x, test_y, name) -> tuple[
     test_feature_matrix, test_features = ft.dfs(
         entityset=test_es,
         target_dataframe_name="test_y",
+        agg_primitives=["sum", "std", "max", "skew", "min", "mean", "count", "percent_true", "num_unique", "mode"],  # default transformations
+        max_depth=10
     )
 
-    print("feature_matrix")
-    print(train_feature_matrix)
-    print("features")
-    print(train_features)
     train_x = train_feature_matrix
-    print("feature_matrix")
-    print(test_feature_matrix)
-    print("features")
-    print(test_features)
     test_x = test_feature_matrix
+
+    print(train_x)
+    print(test_x)
 
     return train_x, test_x
