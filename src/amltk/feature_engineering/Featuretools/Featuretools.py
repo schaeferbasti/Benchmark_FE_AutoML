473f9e6e40ea85e2a09ac9d1b9efd52ee72e71f5
bastis007 <schaefer.bastian@mail.de>
2024-07-02 16:30:33 +0200

    Featuretools method is working, still need to get out features

--- src/amltk/feature_engineering/Featuretools/Featuretools.py
+++ src/amltk/feature_engineering/Featuretools/Featuretools.py
@@ -1,14 +1,34 @@
 # https://github.com/alteryx/featuretools
-
+import numpy as np
 import pandas as pd
 import featuretools as ft
+
 
 def get_xxx_features(train_x, train_y, test_x, test_y, name) -> tuple[
     pd.DataFrame,
     pd.DataFrame
 ]:
-    es = ft.demo.load_mock_customer(return_entityset=True)
-    feature_matrix, feature_defs = ft.dfs(entityset=es, target_dataframe_name=name)
+    # train_y = pd.DataFrame(train_y, columns=["target"])
+    # train_df = pd.concat([train_x, train_y], axis=1)
+    train_x.insert(loc=0, column="id", value=train_x.reset_index().index)
+    train_y = pd.DataFrame(train_y)
+    train_y.insert(loc=0, column="id", value=train_y.reset_index().index)
+    es = ft.EntitySet(id=name)
+    es = es.add_dataframe(
+        dataframe_name=name,
+        dataframe=train_x,
+    )
+    es = es.add_dataframe(
+        dataframe_name="target",
+        dataframe=train_y,
+    )
+    feature_matrix, feature_defs = ft.dfs(
+        entityset=es,
+        target_dataframe_name="target"
+    )
+    print("feature_matrix")
     print(feature_matrix)
+    print("feature_defs")
     print(feature_defs)
+
     return train_x, test_x
