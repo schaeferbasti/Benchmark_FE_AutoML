b7e7ce5d460656b153e93fc13ea6ef8172ead2d4
bastis007 <schaefer.bastian@mail.de>
2024-07-03 16:56:27 +0200

    Boruta runs when using the values of the dataframes

--- src/amltk/feature_engineering/Boruta/Boruta.py
+++ src/amltk/feature_engineering/Boruta/Boruta.py
@@ -5,14 +5,33 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 
 
-def get_xxx_features(train_x, train_y, test_x, ) -> tuple[
+def get_xxx_features(train_x, train_y, test_x) -> tuple[
     pd.DataFrame,
     pd.DataFrame
 ]:
     rf = RandomForestClassifier(n_jobs=-1, class_weight='balanced', max_depth=5)
-
     feat_selector = BorutaPy(rf, n_estimators="auto", verbose=2)
+
+    """
     feat_selector.fit(train_x, train_y)
     train_x = feat_selector.transform(train_x)
     test_x = feat_selector.transform(test_x)
-    return train_x, test_x
+    """
+
+    train_x_np = train_x.values
+    train_y_np = train_y.values
+    test_x_np = test_x.values
+
+    feat_selector.fit(train_x_np, train_y_np.ravel())
+
+    # Transform the training and testing data
+    train_x_selected = feat_selector.transform(train_x_np)
+    test_x_selected = feat_selector.transform(test_x_np)
+
+    # Convert the transformed data back to DataFrame
+    train_x_selected_df = pd.DataFrame(train_x_selected, columns=train_x.columns[feat_selector.support_])
+    test_x_selected_df = pd.DataFrame(test_x_selected, columns=test_x.columns[feat_selector.support_])
+
+    print(train_x_selected_df.columns)
+    print(test_x_selected_df.columns)
+    return train_x_selected_df, test_x_selected_df
