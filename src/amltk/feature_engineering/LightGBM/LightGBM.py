26df567251c77a76ade6bf4cf7f24791b90e85b7
bastis007 <schaefer.bastian@mail.de>
2024-07-12 16:17:10 +0200

    End of LightGBM

--- src/amltk/feature_engineering/LightGBM/LightGBM.py
+++ src/amltk/feature_engineering/LightGBM/LightGBM.py
@@ -1,18 +1,3 @@
 # https://github.com/Microsoft/LightGBM
 
-import pandas as pd
-import lightgbm as lgb
-
-
-def get_xxx_features(train_x, train_y, test_x) -> tuple[
-    pd.DataFrame,
-    pd.DataFrame
-]:
-    categorical_features = []
-    for column in train_x.columns:
-        if train_x[column].dtype == object:
-            categorical_features.append(column)
-
-    data = lgb.Dataset(train_x, label=train_y, feature_name=train_x.columns, categorical_feature=train_x.columns)
-    # No feature engineering
-    return train_x, test_x
+# No Feature Engineering
