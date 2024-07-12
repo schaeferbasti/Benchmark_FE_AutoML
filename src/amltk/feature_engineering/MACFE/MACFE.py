f2b9c2a5c4bc82c1ef0ed89d11c643d7c1b3f636
bastis007 <schaefer.bastian@mail.de>
2024-07-12 16:50:20 +0200

    First try of MACFE

---
+++ src/amltk/feature_engineering/MACFE/MACFE.py
@@ -0,0 +1,18 @@
+# https://github.com/fuyuanlyu/AutoFS-in-CTR/tree/main/LPFS
+import numpy as np
+import pandas as pd
+from pymfe.mfe import MFE
+import scipy
+
+
+
+def get_xxx_features(train_x, train_y, test_x) -> tuple[
+    pd.DataFrame,
+    pd.DataFrame
+]:
+    mfe = MFE()
+    mfe.fit(np.array(train_x), np.array(train_y))
+    train_x = pd.DataFrame(train_x, np.array(mfe.extract()[1]))
+    mfe.fit(np.array(test_x))
+    test_x = pd.DataFrame(test_x, np.array(mfe.extract()[1]))
+    return train_x, test_x
