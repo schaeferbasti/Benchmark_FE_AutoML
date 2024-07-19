605a3e95b7aac159ff5157681d101cebb6953534
bastis007 <schaefer.bastian@mail.de>
2024-07-19 10:56:46 +0200

    Fix CorrelationBasedFS.py by adding factorization of categorical features

--- src/feature_engineering/CorrelationBasedFS/CorrelationBasedFS.py
+++ src/feature_engineering/CorrelationBasedFS/CorrelationBasedFS.py
@@ -10,8 +10,16 @@ def get_correlationbased_features(train_x, train_y, test_x) -> tuple[
     pd.DataFrame,
     pd.DataFrame
 ]:
+    for column in train_x.select_dtypes(include=['object', 'category']).columns:
+        train_x[column], uniques = pd.factorize(train_x[column])
+    for column in test_x.select_dtypes(include=['object', 'category']).columns:
+        test_x[column], uniques = pd.factorize(test_x[column])
+    train_y = pd.DataFrame(train_y)
+    for column in train_y.select_dtypes(include=['object', 'category']).columns:
+        train_y[column], uniques = pd.factorize(train_y[column])
     float_array_of_features = train_x.to_numpy().astype("float64")
     float_array_of_targets = train_y.to_numpy().astype("float64")
+    float_array_of_targets = float_array_of_targets[:, 0]
 
     mufsc = MUFS(discrete=False)
     cfs_f = mufsc.cfs(float_array_of_features, float_array_of_targets).get_results()
