3bd24568f28b5852c7650db112c554e6f6af9892
bastis007 <schaefer.bastian@mail.de>
2024-07-20 14:53:51 +0200

    Minor changes

--- src/autogluon/run_autogluon.py
+++ src/autogluon/run_autogluon.py
@@ -5,22 +5,23 @@ from sklearn.model_selection import train_test_split
 from src.datasets.Splits import get_splits
 
 datasets = ["abalone"]
-methods = ["autofeat", "autogluon", "bioautoml", "boruta", "correlationbased", "featuretools", "featurewiz", "h2o", "macfe", "mafese", "mljar", "openfe"]
+methods = ["autofeat", "autogluon", "bioautoml", "boruta", "correlationbased", "featuretools", "featurewiz", "h2o",
+           "macfe", "mafese", "mljar", "openfe"]
 
 for name in datasets:
     for method in methods:
-        data = TabularDataset(f'src/datasets/feature_engineered_datasets/{name}_{method}.csv')
-        X = data.drop('target', axis=1)
-        y = data['target']
+        data = TabularDataset(f'../datasets/feature_engineered_datasets/{name}_{method}.csv')
+        X = data.iloc[:, :-1]
+        y = data[data.columns[:-1]]
 
         train_x, train_y, test_x, test_y = train_test_split(X, y, test_size=0.1, random_state=42)
-        train_x, train_y, test_x, test_y = get_splits(train_x, train_y, test_x, test_y)
+        # train_x, train_y, test_x, test_y = get_splits(train_x, train_y, test_x, test_y)
         train_data = pd.concat([train_x, train_y], axis=1)
         test_data = pd.concat([test_x, test_y], axis=1)
         train_data = TabularDataset(train_data)
         test_data = TabularDataset(test_data)
 
-        label = train_y.name
+        label = data.columns[:-1]
         predictor = TabularPredictor(label=label).fit(train_data)
 
         y_pred = predictor.predict(test_data.drop(columns=[label]))
