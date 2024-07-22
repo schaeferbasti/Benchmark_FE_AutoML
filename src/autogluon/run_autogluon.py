a26547500d6f23b17180196166211c3f5001802f
bastis007 <schaefer.bastian@mail.de>
2024-07-22 11:24:09 +0200

    First working attempt for autogluon pipeline

--- src/autogluon/run_autogluon.py
+++ src/autogluon/run_autogluon.py
@@ -1,29 +1,41 @@
+import warnings
+
 import pandas as pd
 from autogluon.tabular import TabularDataset, TabularPredictor
-from sklearn.model_selection import train_test_split
 
+from src.datasets.Datasets import preprocess_data, preprocess_target
 from src.datasets.Splits import get_splits
 
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+
 datasets = ["abalone"]
-methods = ["autofeat", "autogluon", "bioautoml", "boruta", "correlationbased", "featuretools", "featurewiz", "h2o",
-           "macfe", "mafese", "mljar", "openfe"]
+methods = ["original", "autofeat"]  # , "autogluon", "bioautoml", "boruta", "correlationbased", "featuretools", "featurewiz", "h2o", "macfe", "mafese", "mljar", "openfe"]
 
 for name in datasets:
     for method in methods:
-        data = TabularDataset(f'../datasets/feature_engineered_datasets/{name}_{method}.csv')
-        X = data.iloc[:, :-1]
-        y = data[data.columns[:-1]]
+        data = pd.read_csv(f'../datasets/feature_engineered_datasets/{name}_{method}.csv')
+        label = data.columns[-1]
 
-        train_x, train_y, test_x, test_y = train_test_split(X, y, test_size=0.1, random_state=42)
-        # train_x, train_y, test_x, test_y = get_splits(train_x, train_y, test_x, test_y)
+        X = data.drop(label, axis=1)
+        y = data[label]
+
+        train_x, train_y, test_x, test_y = get_splits(X, y)
+
+        """
+        train_x, test_x = preprocess_data(train_x, test_x)
+        train_y = preprocess_target(train_y)
+        test_y = preprocess_target(test_y)
+        """
+
         train_data = pd.concat([train_x, train_y], axis=1)
         test_data = pd.concat([test_x, test_y], axis=1)
         train_data = TabularDataset(train_data)
         test_data = TabularDataset(test_data)
 
-        label = data.columns[:-1]
-        predictor = TabularPredictor(label=label).fit(train_data)
+        predictor = TabularPredictor(label=label, verbosity=0).fit(train_data)
 
         y_pred = predictor.predict(test_data.drop(columns=[label]))
-        y_pred.head()
-        predictor.evaluate(test_data, silent=True)
+        print(y_pred.head())
+        eval_dict = predictor.evaluate(test_data, silent=True)
+        print(eval_dict)
