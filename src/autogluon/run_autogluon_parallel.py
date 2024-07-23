8dbad58a22222b76a3928e7e431d20005ec01590
bastis007 <schaefer.bastian@mail.de>
2024-07-23 16:14:04 +0200

    First attempt for parallel run files

---
+++ src/autogluon/run_autogluon_parallel.py
@@ -0,0 +1,83 @@
+import argparse
+import warnings
+import os
+
+import pandas as pd
+from autogluon.tabular import TabularDataset, TabularPredictor
+from sklearn.exceptions import UndefinedMetricWarning
+
+from src.datasets.Datasets import preprocess_data, preprocess_target
+from src.datasets.Splits import get_splits
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action='ignore', category=UndefinedMetricWarning)
+
+def main(args):
+    csv_files = []
+    dataset_file = args.method
+    if dataset_file.endswith(".csv") and not dataset_file.__contains__("exec_times"):
+        csv_files.append(dataset_file)
+        core_name = dataset_file[:-len('.csv')]
+        parts = core_name.split('_')
+        task_hint = parts[0]
+        dataset = parts[1]
+        method = parts[2]
+
+        print(f"\n****************************************\n{dataset} - {method}\n****************************************")
+        execution_times = pd.read_csv(f"../datasets/feature_engineered_datasets/exec_times.csv")
+        result = execution_times[(execution_times['Dataset'] == dataset) & (execution_times['Method'] == method)]
+        # Extract the time value if a match is found
+        if not result.empty:  # method has been executed on dataset
+            exec_time = result['Time'].values[0]
+        else:
+            exec_time = 0  # no FE method executed on dataset -> raw dataset
+
+        time_limit = 14400 - exec_time  # 4h in seconds - time needed for feature engineering
+        max_memory_usage_ratio = 0.1  # share of total memory (we want to give 32GB to autogluon)
+        num_cpus = 8
+
+        print(f"Time limit: {time_limit}")
+        try:
+            data = pd.read_csv(f'../datasets/feature_engineered_datasets/regression_{dataset}_{method}.csv')
+            task_hint = 'regression'
+        except:
+            try:
+                data = pd.read_csv(f'../datasets/feature_engineered_datasets/binary-classification_{dataset}_{method}.csv')
+                task_hint = 'binary'
+            except:
+                data = pd.read_csv(f'../datasets/feature_engineered_datasets/multi-classification_{dataset}_{method}.csv')
+                task_hint = 'multiclass'
+        label = data.columns[-1]
+
+        X = data.drop(label, axis=1)
+        y = data[label]
+
+        train_x, train_y, test_x, test_y = get_splits(X, y)
+
+        train_x, test_x = preprocess_data(train_x, test_x)
+        train_y = preprocess_target(train_y)
+        test_y = preprocess_target(test_y)
+
+        train_data = pd.concat([train_x, train_y], axis=1)
+        test_data = pd.concat([test_x, test_y], axis=1)
+        train_data = TabularDataset(train_data)
+        test_data = TabularDataset(test_data)
+
+        eval_dict = None
+        if task_hint == 'regression':
+            predictor = TabularPredictor(label=label, verbosity=0, problem_type=task_hint, eval_metric="root_mean_squared_error").fit(train_data, time_limit=time_limit, num_cpus=num_cpus, ag_args_fit={max_memory_usage_ratio: max_memory_usage_ratio})
+            eval_dict = predictor.evaluate(test_data)
+        elif task_hint == 'binary':
+            predictor = TabularPredictor(label=label, verbosity=0, problem_type=task_hint, eval_metric="roc_auc").fit(train_data, time_limit=time_limit, num_cpus=num_cpus, ag_args_fit={max_memory_usage_ratio: max_memory_usage_ratio})
+            eval_dict = predictor.evaluate(test_data)
+        elif task_hint == 'multiclass':
+            predictor = TabularPredictor(label=label, verbosity=0, problem_type=task_hint, eval_metric="log_loss").fit(train_data, time_limit=time_limit, num_cpus=num_cpus, ag_args_fit={max_memory_usage_ratio: max_memory_usage_ratio})
+            eval_dict = predictor.evaluate(test_data)
+        print(eval_dict)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Run feature engineering methods')
+    parser.add_argument('--method', type=str, required=True, help='Feature engineering method to use')
+    args = parser.parse_args()
+    main(args)
---
+++ src/autogluon/run_autogluon_parallel.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Define the partition on which the job shall run.
+#SBATCH --partition mlhiwidlc_gpu-rtx2080
+
+# Define a name for your job
+#SBATCH --job-name AMLTK_Pipeline
+
+# Define the files to write the outputs of the job to.
+#SBATCH --output logs/%x-%A_%a.out
+#SBATCH --error logs/%x-%A_%a.err
+
+# Define the amount of memory required per node
+#SBATCH --mem 48GB
+#SBATCH --cpus-per-task=1
+#SBATCH --gres=localtmp:100
+
+#SBATCH --propagate=NONE
+
+# Define job array
+#SBATCH --array=0-1351  # Adjust based on the number of methods
+
+echo "Workingdir: $PWD"
+echo "Started at $(date)"
+
+# SLURM variables
+echo "Running job $SLURM_JOB_NAME using $SLURM_JOB_CPUS_PER_NODE cpus per node with given JID $SLURM_JOB_ID on queue $SLURM_JOB_PARTITION"
+
+# Activate your environment
+source ~/miniconda3/bin/activate
+conda activate amltk_env
+echo "conda amltk_env activated"
+
+# Install requirements
+python3 -m pip install --upgrade pip
+pip install -r requirements.txt
+echo "Requirements installed"
+# shellcheck disable=SC1068
+
+# Set the PYTHONPATH to include the src directory
+export PYTHONPATH=$PWD/src:$PYTHONPATH
+echo "PYTHONPATH set to $PYTHONPATH"
+
+# Define methods array
+directory_path="../datasets/feature_engineered_datasets/"
+
+# List all files in the directory and store them in an array
+methods=($(ls "$directory_path"))
+# Get the method name based on SLURM_ARRAY_TASK_ID
+method=${methods[$SLURM_ARRAY_TASK_ID]}
+
+# Run the job with the specified method
+start=`date +%s`
+
+echo "Running Method: $method"
+python3 run_feature_engineering_parallel.py --method $method
+
+end=`date +%s`
+runtime=$((end-start))
+
+echo "Job execution complete."
+echo "Runtime: $runtime"
