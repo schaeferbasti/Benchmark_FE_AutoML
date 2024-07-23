fda5211230b9bd7d3c6fe0b50da25006aaa4b6c7
bastis007 <schaefer.bastian@mail.de>
2024-07-23 11:04:34 +0200

    Introduce metrics for regression and classification

--- src/autogluon/run_autogluon.py
+++ src/autogluon/run_autogluon.py
@@ -2,18 +2,21 @@ import warnings
 
 import pandas as pd
 from autogluon.tabular import TabularDataset, TabularPredictor
+from sklearn.exceptions import UndefinedMetricWarning
 
 from src.datasets.Datasets import preprocess_data, preprocess_target
 from src.datasets.Splits import get_splits
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action='ignore', category=UndefinedMetricWarning)
 
-
+task_hint = 'regression'
 datasets = ["abalone"]
-methods = ["original", "autofeat"]  # , "autogluon", "bioautoml", "boruta", "correlationbased", "featuretools", "featurewiz", "h2o", "macfe", "mafese", "mljar", "openfe"]
+methods = ["original", "autofeat", "autogluon"]  # , "bioautoml", "boruta", "correlationbased", "featuretools", "featurewiz", "h2o", "macfe", "mafese", "mljar", "openfe"]
 
 for name in datasets:
     for method in methods:
+        print(f"\n****************************************\n{name} - {method}\n****************************************")
         data = pd.read_csv(f'../datasets/feature_engineered_datasets/{name}_{method}.csv')
         label = data.columns[-1]
 
@@ -30,7 +33,13 @@ for name in datasets:
         test_data = pd.concat([test_x, test_y], axis=1)
         train_data = TabularDataset(train_data)
         test_data = TabularDataset(test_data)
-
-        predictor = TabularPredictor(label=label, verbosity=0).fit(train_data)
-        eval_dict = predictor.evaluate(test_data, silent=True)
+        if task_hint == 'regression':
+            predictor = TabularPredictor(label=label, verbosity=0, eval_metric="root_mean_squared_error").fit(train_data)
+            eval_dict = predictor.evaluate(test_data)
+        elif task_hint == 'binary_classification':
+            predictor = TabularPredictor(label=label, verbosity=0, eval_metric="roc_auc").fit(train_data)
+            eval_dict = predictor.evaluate(test_data)
+        elif task_hint == 'multi_classification':
+            predictor = TabularPredictor(label=label, verbosity=0, eval_metric="log_loss").fit(train_data)
+            eval_dict = predictor.evaluate(test_data)
         print(eval_dict)
