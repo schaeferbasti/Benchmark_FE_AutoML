9d4906bccf10cacfbb77e9295b55a5a0577d43bf
bastis007 <schaefer.bastian@mail.de>
2024-09-02 16:41:27 +0200

    Also use fcbf

--- src/feature_engineering/CorrelationBasedFS/CorrelationBasedFS.py
+++ src/feature_engineering/CorrelationBasedFS/CorrelationBasedFS.py
@@ -21,11 +21,11 @@ def get_correlationbased_features(train_x, train_y, test_x) -> tuple[
     float_array_of_targets = train_y.to_numpy().astype("float64")
     float_array_of_targets = float_array_of_targets[:, 0]
 
-    mufsc = MUFS(discrete=False)
-    cfs_f = mufsc.cfs(float_array_of_features, float_array_of_targets).get_results()
-
+    mufs = MUFS(discrete=False)
+    cfs_f = mufs.cfs(float_array_of_features, float_array_of_targets).get_results()
+    fcbf_f = mufs.fcbf(float_array_of_features, float_array_of_targets, 1e-3).get_results()
     print(cfs_f)
-    print(mufsc.get_scores())
+    print(mufs.get_scores())
 
     train_x = train_x.iloc[:, cfs_f]
     test_x = test_x.iloc[:, cfs_f]
