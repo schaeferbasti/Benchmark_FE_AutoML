41edb819e71349734869b7ea6c5211f917068e3c
bastis007 <schaefer.bastian@mail.de>
2024-09-10 10:53:18 +0200

    Update script to move fe datasets

--- src/datasets/feature_engineered_datasets/move_datasets.py
+++ src/datasets/feature_engineered_datasets/move_datasets.py
@@ -5,20 +5,21 @@ for filename in os.listdir():
     if filename.endswith(".parquet"):
         # Split the filename to extract type, dataset, method, and fold
         parts = filename.split('_')
-        if len(parts) >= 4:
-            dataset = parts[1]  # e.g., 'abalone'
-            method = parts[2]  # e.g., 'openfe' or 'original'
+        method = parts[-2]
+        parts.remove(method)
+        parts.remove(parts[-1])
+        dataset = "_".join(parts)
 
-            # Create a new folder path based on dataset and method
-            new_folder = f"{dataset}_{method}"
+        # Create a new folder path based on dataset and method
+        new_folder = f"{dataset}_{method}"
 
-            # Ensure the folder exists
-            os.makedirs(new_folder, exist_ok=True)
+        # Ensure the folder exists
+        os.makedirs(new_folder, exist_ok=True)
 
-            # Define the source and destination paths
-            source_path = os.path.join(filename)
-            destination_path = os.path.join(new_folder, filename)
+        # Define the source and destination paths
+        source_path = os.path.join(filename)
+        destination_path = os.path.join(new_folder, filename)
 
-            # Move the file to the new folder
-            shutil.move(source_path, destination_path)
-            print(f"Moved {filename} to {new_folder}")
+        # Move the file to the new folder
+        shutil.move(source_path, destination_path)
+        print(f"Moved {filename} to {new_folder}")
